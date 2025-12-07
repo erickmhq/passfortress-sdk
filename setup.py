fffb350254bd8647a26e51727015a2c4f364919b
Erick M. Hernández Quiñones <erickmhq@gmail.com>
2025-12-07 01:18:46 +0100

    Update host configuration and bump version to 2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='passfortress-sdk',
-    version='1.1.0',
+    version='2.0.0',
     packages=find_packages(),
     install_requires=[
         'requests==2.32.3'
