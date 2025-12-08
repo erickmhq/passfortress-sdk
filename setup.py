be52be6d51f1660923cfee65e0c9457bb7e3f8a3
Erick M. Hernández Quiñones <erickmhq@gmail.com>
2025-12-08 13:12:21 +0100

    Bump version to 3.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='passfortress-sdk',
-    version='2.0.0',
+    version='3.0.0',
     packages=find_packages(),
     install_requires=[
         'requests==2.32.3'
